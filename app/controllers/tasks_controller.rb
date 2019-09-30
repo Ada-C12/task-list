d0b95fb45029717cd2b9da442ed58b424e96e7a9
KKennedyCodes <holmstedtk@gmail.com>
2019-09-30 16:26:35 -0700

    Baseline Complete, Gemfiles Added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
+TASKS = [
+{ task: "Pay Rent", description: "$2,100, Send Check by the First"},
+{ task: "Donate Items", description: "Deliver to Goodwill in Ballard"},
+{ task: "Send Thank You", description: "Mail Card by Friday"}
+]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+  
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end
