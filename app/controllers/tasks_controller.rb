e20e30d4ca4b630d8858364dae2a9457aa744bbd
Ga-Young Jin <gyjinn@gmail.com>
2019-09-30 15:19:12 -0700

    added hard-coded data, controller action to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
+TASKS = [
+  { name: "mail package", description: "send to Milton", completion_date: Time.now + 3.days},
+  { name: "clean kitchen", description: "wash dishes, mop floor", completion_date: Time.now + 1.days},
+  { name: "complete homework", description: "task list project", completion_date: Time.now + 7.days},
+  { name: "feed dog", description: "treats to Max", completion_date: Time.now + 3.hours}
+]
+
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end
