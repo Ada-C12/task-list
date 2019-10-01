c6f64ac459fd082e6b6cccb084728ccc19aa42dc
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-01 16:32:45 -0700

    Changed completion_date column in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_042221) do
+ActiveRecord::Schema.define(version: 2019_10_01_232235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2019_10_01_042221) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completion_date"
   end
 
 end
