64fb7d24e91224db04bccb91587b8b1da66a0ed0
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-01 16:15:21 -0700

    Updated tasks_controller#index to use .all and update the view as well.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,5 @@
-TASKS = [
-  {name: 'wash the dishes', status: 'incomplete'},
-  {name: 'walk the dog', status: 'incomplete'},
-  {name: 'vacuum', status: 'incomplete'}
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
