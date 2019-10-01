d4aa57db8bf50c56ad33fe263471b35faccdda94
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-01 16:00:19 -0700

    Created Task model.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
+TASKS = [
+  {name: 'wash the dishes', status: 'incomplete'},
+  {name: 'walk the dog', status: 'incomplete'},
+  {name: 'vacuum', status: 'incomplete'}
+]
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end
