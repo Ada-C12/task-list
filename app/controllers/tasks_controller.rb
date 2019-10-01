32861174fd7b22eac963c6fcae1031b73844a1f5
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-01 15:52:22 -0700

    Created link to adding a new task, and created new and create actions/methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,16 @@ class TasksController < ApplicationController
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    form_data = params["task"]
+    name = form_data["name"]
+    description = form_data["description"]
+
+    @task = Task.new(name: name, description: description)
+  end
 end
