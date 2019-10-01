465c62db0054dd8216986e632898059f5607d125
Hallie <idhallie@gmail.com>
2019-10-01 16:29:12 -0700

    Ran db:migrate and added 3 tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,16 @@
-TASKS = ["make bed", "get groceries", "walk dog", "feed dog", "cuddle dog"]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+  
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    
+    if @task.nil?
+      head :not_found
+      return
+    end
   end
 end
