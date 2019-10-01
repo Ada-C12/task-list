5bc8bd94c31c6b45cc5aa7c1d221de06994df775
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-01 15:44:25 -0700

    updated TaskController#index to use the new task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,5 @@
-TASKS = ["save city from evil dragon", "find the lost hero's sword", "defeat arch-nemesis's minions", "defeat arch-nemesis", "rescue cat stuck in tree"]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
