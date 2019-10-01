c49c6d8b3d343f36626a622ad93303305dca4523
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-01 16:44:25 -0700

    Added model for task, updated index in task_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,14 @@ class TasksController < ApplicationController
   
   def index
     @greeting = "HELLO, THERE!"
+    @tasks = Task.all
   end
   
   def show
+    @tasks = Task.all
     @task_id = params[:id].to_i
-    @spookymode = false
-    @tasks = ["task", "task2", "task3"]
-    @task = @tasks[@task_id]
+    # @spookymode = false
+    @task = @tasks.find_by(id: @task_id)
     if @task.nil?
       head :not_found
       return
