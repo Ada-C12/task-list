e74a73b019b39378c6de5e1983651628dbf9cda1
Dianna Tingg <dtingg@gmail.com>
2019-10-01 16:07:40 -0700

    fixed data type for completion date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_223517) do
+ActiveRecord::Schema.define(version: 2019_10_01_230427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,9 @@ ActiveRecord::Schema.define(version: 2019_10_01_223517) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "completion_date"
-    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "completion_date"
   end
 
 end
