56844764e919c25f6a1bc87b879083c95bf17756
Janice Huang <janicehuang@gmail.com>
2019-10-01 16:00:31 -0700

    add model, four columns to table, two instances

---
+++ db/migrate/20191001222713_add_description_to_tasks.rb
@@ -0,0 +1,8 @@
+class AddDescriptionToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :name, :string
+    add_column :tasks, :description, :string
+    add_column :tasks, :assignment_date, :date
+    add_column :tasks, :completion_date, :date
+  end
+end
