23b82d306795ee3a57ee9cdf3f1f083fe04b4016
Kristina <kristina.tanya@gmail.com>
2019-09-30 19:13:02 -0700

    Passing all Wave 2 tests except redirect and flash message for invalid task path

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,27 +3,41 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
   
+  # show the task by the id number
   def show
     @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      
+      # return
+      flash[:notice] = "Could not find task with id: -1"
+      redirect_to tasks_path #:flash => { :notice => "Could not find task with id: -1" } 
+      
+    end
   end
   
+  # generate a new task
   def new
     @task = Task.new
   end
   
+  # submit and save the new task
   def create
     @task = Task.new(task_params)
     
     if @task.save
-      redirect_to tasks_path
+      # DELETE COMMENT ONCE INSTRUCTOR PROVIDES CLARIFICATION
+      # redirect_to tasks_path
+      redirect_to @task
+      return
     else
       render :new
+      return
     end
   end
   
   private
   
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:id, :name, :description, :completed_at)
   end
 end
