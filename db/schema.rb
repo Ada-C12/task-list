b990bf65162a8d5573c47cdf4fb801e1570ec5f3
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-02 09:46:06 -0700

    Changed due_date back to completion_date for the tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_234454) do
+ActiveRecord::Schema.define(version: 2019_10_02_164339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_234454) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.date "due_date"
+    t.date "completion_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
