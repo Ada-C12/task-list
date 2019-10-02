f4d551de9240c0166ce0f12006d3b09fd15f7608
Alice <alicesunhi@gmail.com>
2019-10-02 10:44:49 -0700

    added show erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
   # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
