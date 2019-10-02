f7a7a7eb132175a1eb9bbde3cfdcc7f6f81455e2
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-02 16:06:38 -0700

    fixed completion_date/completed inconsistency, removed due date expectation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   
   root "tasks#index"
   
-  # verb 'path', to: 'controller#action'
+  # Routes that operate on the task collection
   get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  
+  # Routes that operate on individual tasks
+  get '/task/:id', to: 'task#show', as: 'task'
+  
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/tasks/:id', to: 'tasks#update'
+  
 end
