a7958d3714e7644f7f3d7b4336527492a244df87
Eve Le <nguyenthao.lee@gmail.com>
2019-10-01 23:27:06 -0700

    Generate new model, create database and manually insert tasks' info to database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,12 @@
-TASKS = [
-    "Finish homework",
-    "Do laundry",
-    "Water plants",
-    "Feed Tofu",
-    "Walk Tofu"
-]
-
 class TasksController < ApplicationController
     def index
-        @tasks = TASKS
+        @tasks = Task.all
     end
     
     def show
         task_id_raw = params[:id]
         task_id = task_id_raw.to_i
-        @task = TASKS[task_id]
+        @task = Task.all[task_id]
         if task_id.to_s != task_id_raw || !@task
             head :not_found
             return
