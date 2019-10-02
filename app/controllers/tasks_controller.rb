7eebea4319beec5df5b9dbaebb4e12cc58dcbf10
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-02 00:27:05 -0700

    Wave1 Complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,25 @@
 TASKS = ["Laundry", "Feed Babette", "Clean room", "Sleep"] 
-
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
-
-
+  
+  def show
+    task_id = params[:index].to_i
+    @task = Task.find(index: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end
+  
+# def show
+#   task_id = params[:index].to_i
+#   @task = TASKS[task_id]
+#   if @task.nil?
+#     head :not_found
+#     return
+#   end
+# end
