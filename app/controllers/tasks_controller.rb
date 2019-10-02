85bd5665fbcca8e76f7eff4c3bc510d4b719111a
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-02 07:39:36 -0700

    completed baseline, wave 1, and the show method of wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,21 @@
 class TasksController < ApplicationController
   
-  TASKS = %w[laundry dishes homework mail gardening]
   def index 
-    @tasks = TASKS
+    @tasks = Task.all
   end
+  
+  def show 
+    task_name = params[:name]
+    @task = Task.find_by(name: task_name)
+    
+    if @task.nil?
+      head:not_found
+      return 
+    end 
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: 'root'
+  get '/tasks/:name', to: 'tasks#show'
 end
