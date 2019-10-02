f43f96f81121e33fa1a231109f1df79221c96514
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-02 14:04:05 -0700

    fixed flash message to use interpolation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
     
     if @task.nil?
-      redirect_to root_path, flash: { error: "Could not find task with id: -1" }
+      redirect_to root_path, flash: { error: "Could not find task with id: #{task_id}" }
       return
     end
   end
