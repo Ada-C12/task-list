a3bd13c58961142ebb085d808acec01f76b98b9c
Dominique <dom_taylor@icloud.com>
2019-10-01 20:50:41 -0700

    changed completion date attribute on Task model instances to a string data type

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,3 @@
-TASKS = ["clean your room", "clean the bathroom", "clean the kitchen"]
-
 class TasksController < ApplicationController
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_231228) do
+ActiveRecord::Schema.define(version: 2019_10_02_034102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_231228) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
-    t.datetime "completion_date"
+    t.string "completion_date"
   end
 
 end
