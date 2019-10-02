13d3c0ca8c079702014660f2864b34f25643767b
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-01 19:11:26 -0700

    Added tasks#update and can update a specific task.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,12 +7,10 @@ class TasksController < ApplicationController
   
   def show
     task_id = params[:id].to_i
-    @tasks = Task.find_by(id:task_id)
-    if @tasks.nil?
+    @task = Task.find_by(id:task_id)
+    if @task.nil?
       flash[:error] = "Could not find task with id: #{task_id}"
       redirect_to tasks_path
-      
-      # return
     end
   end
   
@@ -26,6 +24,32 @@ class TasksController < ApplicationController
     task.save
     
     redirect_to tasks_path
+  end
+  
+  def edit
+    task_id = params[:id].to_i
+    @task = Task.find_by(id:task_id)
+    if @task.nil?
+      flash[:error] = "Could not find task with id: #{task_id}"
+      redirect_to tasks_path
+    end
+  end
+  
+  def update
+    task_id = params[:id].to_i
+    task = Task.find_by(id:task_id)
+    if task.nil?
+      flash[:error] = "Could not find task with id: #{task_id}"
+      redirect_to tasks_path
+      return
+    end
+    
+    task_params = params[:task]
+    task.name = task_params[:name]
+    task.description = task_params[:description]
+    task.save
+    
+    redirect_to task_path(task)
   end
   
 end
