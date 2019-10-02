4c4bef9497beac7833a565a7fc880e80bc9810da
Eve Le <nguyenthao.lee@gmail.com>
2019-10-01 17:05:10 -0700

    Add show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,14 @@ class TasksController < ApplicationController
     def index
         @tasks = TASKS
     end
+    
+    def show
+        task_id_raw = params[:id]
+        task_id = task_id_raw.to_i
+        @task = TASKS[task_id]
+        if task_id.to_s != task_id_raw || !@task
+            head :not_found
+            return
+        end
+    end
 end
