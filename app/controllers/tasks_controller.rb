00ce23f1268a072a01e4ff3e2b8b9ae203406a67
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-02 12:27:48 -0700

    added show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    
+    if @task.nil?
+      redirect_to root_path, flash: { error: "Could not find task with id: -1" }
+      return
+    end
+  end
 end
