edf6d3f507170924af1a42056c717d03a16be637
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-02 16:18:46 -0700

    Added 'new' view, 'new' method in controller, 'new' route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,9 +11,27 @@ class TasksController < ApplicationController
     @task = @tasks.find_by(id: task_id)
     if @task.nil?
       flash[:error] = "Could not find task with id: " + task_id.to_s
-      redirect_to root_path
+      redirect_to tasks_path
       return
     end
   end
+  
+  def new
+    @task = Task.new
+    @task.completion_date = Date.today + 5
+  end
+  
+  def create
+    @task = Task.new( name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date] )
+    
+    @task.save
+    
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render new_task_path
+    end
+  end
+  
   
 end
