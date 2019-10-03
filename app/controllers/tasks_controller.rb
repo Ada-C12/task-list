94974a8281fe1b7f9a9bd28113a991fcc8b623ad
m.dove@live.com <m.dove@live.com>
2019-10-02 17:01:50 -0700

    Updated with Jared's test updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
   # TASKS = [
-  #   { name: " Watch CS Videos", description: "Watch Video Chris POsted", due: "10/03/19" },
-  #   { name: "Rails Tasks Project", description: "Do all waves of tasks", due: "10/07/19" },
-  #   { name: "POODR Reading", description: "Read POODR", due: "10/05/19" },
+  #   { name: " Watch CS Videos", description: "Watch Video Chris POsted", completed: "10/03/19" },
+  #   { name: "Rails Tasks Project", description: "Do all waves of tasks", completed: "10/07/19" },
+  #   { name: "POODR Reading", description: "Read POODR", completed: "10/05/19" },
   # ]
 
   def index
@@ -18,8 +18,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], due: params[:task][:due])
-    if new_task.save
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed: params[:task][:completed])
+    if @task.save
       redirect_to task_path(@task.id)
     else
       render new_task_path
@@ -28,5 +28,22 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.name = params[:book][:name]
+    @task.description = params[:book][:description]
+    @task.completed = params[:book][:description]
+
+    if new_task.save
+      redirect_to task_path(@task.id)
+    else
+      render new_task_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_232207) do
+ActiveRecord::Schema.define(version: 2019_10_02_231738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_232207) do
     t.datetime "due"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "completed"
   end
 
 end
