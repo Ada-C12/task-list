b963c965ccf9ebe00d18790bcde2387bdfbab624
geli-gel <angele.zam@gmail.com>
2019-10-03 16:21:16 -0700

    ignoring due_date for now - still in schema though

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,8 @@ class TasksController < ApplicationController
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
+      # head :not_found
       return
     end
   end
@@ -16,4 +17,21 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    form = params["task"]
+    @new_task = Task.new
+
+    @new_task.name = form["name"]
+    @new_task.description = form["description"]
+    # form_date = "#{form["due_date(2i)"]}/#{form["due_date(3i)"]}/#{form["due_date(1i)"]}"
+    # new_task.due_date = DateTime.parse(Date.parse(form_date).strftime("%Y/%-d/%-m"))
+
+    @new_task.status = "INCOMPLETE"
+    
+    if @new_task.save
+      redirect_to task_path( @new_task.id )
+    else
+
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  post '/tasks', to: 'tasks#create'
   
 end
