7d562bd43d7719916b285b20a94b6962f34bd4d4
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-03 14:10:19 -0700

    Wrote #new and #create to make new task. Created new.html.erb for form. Wrote new routes.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,4 +12,19 @@ class TasksController < ApplicationController
       return
     end
   end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
 
 
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
