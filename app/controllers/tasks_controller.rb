1c9477327a590dc5f01e0985fa8095307ed9f5d4
Eve Le <nguyenthao.lee@gmail.com>
2019-10-03 13:26:13 -0700

    Add test and implement update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
         task_id = task_id_raw.to_i
         @task = Task.find_by(id: task_id)
         if task_id.to_s != task_id_raw || @task.nil?
-            redirect_to root_path
+            redirect_to tasks_path
             return
         end
     end
@@ -18,12 +18,42 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new( name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date] )
+        hash_info = {
+            name: params[:task][:name],
+            description: params[:task][:description],
+            completion_date: params[:task][:completion_date]
+        }
         
+        @task = Task.new(hash_info)
         if @task.save
             redirect_to task_path(@task.id)
         else
             render new_book_path
         end
+    end
+    
+    def edit
+        @task = Task.find_by(id: params[:id])
+        if @task.nil?
+            redirect_to tasks_path
+            return
+        end
+    end
+    
+    def update
+        @task = Task.find_by(id: params[:id])
+        if @task
+            @task.name = params[:task][:name]
+            @task.description = params[:task][:description]
+            @task.completion_date = params[:task][:completion_date]
+            
+            if @task.save
+                redirect_to task_path(@task.id)
+            else
+                render new_task_path
+            end
+        else
+            redirect_to tasks_path
+        end  
     end
 end
