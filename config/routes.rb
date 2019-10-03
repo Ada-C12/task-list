77d82238c2dcd5525b191c0aefba188eabe6b3f9
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-10-02 20:45:51 -0700

    added tasks_path and root_path to router, wave 1 tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tasks", to: "tasks#index"
+  root 'tasks#index'
+  get "/tasks", to: "tasks#index", as: 'tasks'
 end
+
+
+
+
+#   # Routes that operate on the book collection
+#   root 'books#index'
+#   get '/books', to: 'books#index', as: 'books'
+#   get '/books/new', to: 'books#new', as: 'new_book'
+#   post '/books', to: 'books#create'
+
+#   # Routes that operate on individual books
+#   get '/books/:id', to: 'books#show', as: 'book'
+#   get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+#   patch '/books/:id', to: 'books#update'
+#   delete '/books/:id', to: 'books#destroy'
+
