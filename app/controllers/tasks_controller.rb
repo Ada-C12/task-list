7528ba82ccb3dc23d2237e893be5aba5c874aafb
ohcloud <cloudylopez@icloud.com>
2019-10-02 17:51:22 -0700

    updated some functions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
   
   def show
     task_id = params[:id]
-    @task = Task.where(id: task_id)
+    @task = Task.find_by(id: task_id)
     
     if @task.nil?
       head :not_found
@@ -14,4 +14,43 @@ class TasksController < ApplicationController
     end
   end
   
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(name: params[:task][:description], description: params[:task][:description], completion_date: params[:task][:completion_date]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :new # show the new task form view again
+      return
+    end
+  end
+  
+  def edit
+    @task = Task.find_by(id: params[:id])
+    
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.update(
+      name: params[:task][:name], 
+      description: params[:task][:description], 
+      completion_date: params[:task][:completion_date]
+    )
+    redirect_to tasks_path # go to the index so we can see the task in the list
+    return
+  else # save failed :(
+    render :edit # show the new task form view again
+    return
+  end
+end
+
 end
