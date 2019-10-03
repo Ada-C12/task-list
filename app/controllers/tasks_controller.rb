4c52a2e42419d7ed6347bcc7f8211c26d732c9a6
Eve Le <nguyenthao.lee@gmail.com>
2019-10-02 18:01:09 -0700

    Implement show and create for Task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,10 +6,24 @@ class TasksController < ApplicationController
     def show
         task_id_raw = params[:id]
         task_id = task_id_raw.to_i
-        @task = Task.all[task_id]
-        if task_id.to_s != task_id_raw || !@task
-            head :not_found
+        @task = Task.find_by(id: task_id)
+        if task_id.to_s != task_id_raw || @task.nil?
+            redirect_to root_path
             return
+        end
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new( name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date] )
+        
+        if @task.save
+            redirect_to task_path(@task.id)
+        else
+            render new_book_path
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "tasks#index"
+  
+  get "/tasks", to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  
+  get "/tasks/:id", to: "tasks#show", as: "task"
 
-  get "/tasks", to: "tasks#index"
-
-  get "/tasks/:id", to: "tasks#show"
+  post "/tasks", to: "tasks#create"
 end
