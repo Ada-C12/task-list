fc0856ee689e65cebfac89a6f9b5fdcfe2bbe737
Hallie <idhallie@gmail.com>
2019-10-02 20:05:08 -0700

    Created an action, route, and view for a New task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,10 +6,25 @@ class TasksController < ApplicationController
   
   def show
     task_id = params[:id]
-    @task = Task.find(task_id)
+    @task = Task.find_by(id: task_id)
     
     if @task.nil?
       head :not_found
+      return
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    if @task.save
+      redirect_to root_path 
+      return
+    else 
+      render :new 
       return
     end
   end
