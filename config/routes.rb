9c09a7b3c10a30cfb2ced918dfe8f1b391142560
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-03 14:16:56 -0700

    completion_date updated on files

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 # Routes that operate on individual tasks
   get '/tasks/:id', to: 'tasks#show', as: "task"
 
-  get '/tasks/edit/:id', to: 'tasks#edit', as: 'task_edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
   post '/tasks/update/:id', to: 'tasks#update', as: 'task_update'
 
