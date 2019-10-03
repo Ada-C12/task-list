f9409096936407b708f85809804dba5c05b5a4f7
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-02 17:37:07 -0700

    added route to tasks#new and tasks#create

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   
   # Routes that operate on the task collection
   get '/tasks', to: 'tasks#index', as: 'tasks'
-  
-  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  # post '/tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
   
   # Routes that operate on individual tasks
   get '/task/:id', to: 'task#show', as: 'task'
