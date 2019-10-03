8cb84b65a9bda9f626b621e8b69ac635537e6c8d
Mira <calopter@gmail.com>
2019-10-03 14:11:33 -0700

    adds new route action and view for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,8 @@ class TasksController < ApplicationController
   def show
     @task = Task.find_by(id: params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   
   get '/tasks', to: 'tasks#index'
   
+  get '/tasks/new', to: 'tasks#new'
+  
   get '/tasks/:id', to: 'tasks#show'
 end
