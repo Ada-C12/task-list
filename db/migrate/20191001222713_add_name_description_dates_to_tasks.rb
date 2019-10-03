ad29ba7ace745a76aeff27dc0df45fe4abc6c38e
Janice Huang <janicehuang@gmail.com>
2019-10-02 21:10:47 -0700

    start wave 2

--- db/migrate/20191001222713_add_name_description_dates_to_tasks.rb
+++ db/migrate/20191001222713_add_name_description_dates_to_tasks.rb
@@ -3,6 +3,6 @@ class AddDescriptionToTasks < ActiveRecord::Migration[5.2]
     add_column :tasks, :name, :string
     add_column :tasks, :description, :string
     add_column :tasks, :assignment_date, :date
-    add_column :tasks, :completion_date, :date
+    add_column :tasks, :completion_date, :date  
   end
 end
