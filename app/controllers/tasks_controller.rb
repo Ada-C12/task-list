853fe6ad4d067185fcd2c5569c3e6d23bbcc71d6
gracexinran <gracexinran@gmail.com>
2019-10-02 22:53:16 -0700

    created Task model and schema; created different requests in routes; created index, show, new, create, edit and update controller methods; created views for index, show, new and edit methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,50 @@
-TASKS = [
-{ title: "Hidden Figures", author: "Margot Lee Shetterly"},
-{ title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-{ title: "Kindred", author: "Octavia E. Butler"}
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
+  
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed: params[:task][:completed])
+    
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render new_task_path
+    end
+  end
+  
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+  
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.name = params[:task][:name]
+    @task.description = params[:task][:description]
+    @task.completed = params[:task][:completed]
+    
+    if @task.save
+      redirect_to tasks_path
+    else
+      render new_task_path
+    end
+    
+  end
+  
+  def destroy
+    
+  end
+  
 end
