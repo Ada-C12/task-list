80749382d9872b6ca5bcb1a063ac8f03f6c52420
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-10-02 20:39:11 -0700

    completed wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,23 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
     
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path, :flash => { :error => "Could not find task with id: #{task_id}" }
+      return
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new
       return
     end
   end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -47,8 +47,6 @@ describe TasksController do
   
   describe "new" do
     it "can get the new task page" do
-      skip
-      
       # Act
       get new_task_path
       
@@ -58,9 +56,7 @@ describe TasksController do
   end
   
   describe "create" do
-    it "can create a new task" do
-      skip
-      
+    it "can create a new task" do  
       # Arrange
       task_hash = {
         task: {
@@ -77,8 +73,7 @@ describe TasksController do
       
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
-      expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
-      expect(new_task.completed).must_equal task_hash[:task][:completed]
+      expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
       
       must_respond_with :redirect
       must_redirect_to task_path(new_task.id)
