868ff9ad9b334e6b80981397954af9cd9bf2cce1
Monick <monick.keo@gmail.com>
2019-10-02 21:14:03 -0700

    added new and create routes, actions, and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,10 +6,24 @@ class TasksController < ApplicationController
 
   def show
     task_id = params[:id].to_i
-    @task = Task.find_by(id:task_id)
+    @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
+      redirect_to task_path
       return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end 
+
+  def create
+    @task = Task.new( name: params[:task][:name], description: params[:task][:description], completed: params[:task][:completed])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render new_task_path
     end
   end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   #routes for entire task list 
   get '/tasks', to: 'tasks#index', as: 'tasks'
-
-
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
 
   #routes for individual tasks 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
