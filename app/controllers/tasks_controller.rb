c5b530dca3637d549c1c125bf8d765f9e2bfd799
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-02 17:01:34 -0700

    completed wave 2 and passed tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,8 +12,22 @@ class TasksController < ApplicationController
     
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
-      return
+      redirect_to tasks_path
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render new_task_path
+    end
+  end
+
 end
