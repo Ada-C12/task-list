ba7ecaeb564f925fd642a5d725f8f9b5c28b2d81
Morgan Schuler <morganlschuler@gmail.com>
2019-10-03 09:17:46 -0700

    Updated wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,31 @@ class TasksController < ApplicationController
     @task = Task.find_by(id:task_id)
     
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
   end
   
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    task_params = params[:task]
+    task = Task.new(name: task_params[:name], description: task_params[:description])
+    task.save
+    
+    redirect_to task_path(task)
+  end
 end
+
+# def create
+#   @book = Book.new(author: params[:book][:author], title: params[:book][:title], description: params[:book][:description]) #instantiate a new book
+#   if @book.save # save returns true if the database insert succeeds
+#     redirect_to books_path # go to the index so we can see the book in the list
+#     return
+#   else # save failed :(
+#     render :new # show the new book form view again
+#     return
+#   end
+# end
