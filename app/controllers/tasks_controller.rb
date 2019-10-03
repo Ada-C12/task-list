68e77305e97520b64bab8e4632e803361d34d6b3
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-02 17:51:30 -0700

    wave 2 finished & successfully tested, pending further changes from instructors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,55 @@
 class TasksController < ApplicationController
   
-  
-  
   def index
     @tasks = Task.all 
+  end
+  
+  def show
+    user_input = params[:id]
+    if user_input.to_i.to_s == user_input
+      # if input is a valid integer
+      @task = Task.find_by(id: params[:id])
+      unless @task
+        # if id# doesn't exist
+        redirect_to root_path
+      end
+    else 
+      # if input is not an integer
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    # User wants to make a new Task, so we'll need to send user to empty form
+    @task = Task.new
+    # @task.completion_date = "Pending"
+  end
+  
+  def create
+    # This is AFTER user submits form to make new Task
+    @task = Task.new(params_from_form)
+    @error_msg = ""
+    
+    # validate parameters
+    invalid_form = false
+    if @task.name == ""
+      @error_msg = "You must name your task"
+      # elsif @task.completion_date 
+      #parse the comp date soemhow
+    else
+      if @task.save
+        redirect_to task_path(@task.id)
+      else
+        render tasks_new_path(@task)
+      end
+    end
+  end
+  
+  
+  private
+  def params_from_form
+    return params.require(:task).permit(:name, :description, :completion_date)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+  
   get '/tasks', to: 'tasks#index'
   
-  root 'tasks#index'
+  # I think normall resources :tasks will take care of all the routes for me...?
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  
+  
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
