2f65d7ccff5a91311cc37585d87952f73a02cba3
Dominique <dom_taylor@icloud.com>
2019-10-02 20:43:10 -0700

    Wave 1 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_034102) do
+ActiveRecord::Schema.define(version: 2019_10_03_033012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_034102) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
-    t.string "completion_date"
+    t.datetime "completed"
   end
 
 end
