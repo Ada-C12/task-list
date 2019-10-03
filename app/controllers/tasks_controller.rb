c9ab9fd02882141f18aa61e802cb231d84b05b54
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-02 18:07:12 -0700

    added create method and fixed show route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    task_id = params[:id]
+    task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     
     if @task.nil?
@@ -15,6 +15,21 @@ class TasksController < ApplicationController
   
   def new
     @task = Task.new
+  end
+  
+  def create
+    # instantiate a new Task based on form input
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    
+    # if the task can be saved to the database,
+    if @task.save
+      # go to that task's page
+      redirect_to task_path(@task.id)
+      return
+    else
+      # otherwise, show the new task form again
+      render :new
+    end
   end
   
 end
