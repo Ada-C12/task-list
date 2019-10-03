799cdf20e6d8ef373441edb7cee09fe26316592d
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-03 14:30:11 -0700

    Refactored to pass tests. Wave 2 done, passes all tests.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
   end
@@ -18,9 +18,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: nil)
     if @task.save
-      redirect_to tasks_path
+      redirect_to task_path(@task.id)
       return
     else
       render :new
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -28,7 +28,7 @@ end
 # Unskip these tests for Wave 2
 describe "show" do
   it "can get a valid task" do
-    skip
+    
     # Act
     get task_path(task.id)
     
@@ -37,7 +37,7 @@ describe "show" do
   end
   
   it "will redirect for an invalid task" do
-    skip
+    
     # Act
     get task_path(-1)
     
@@ -49,7 +49,7 @@ end
 
 describe "new" do
   it "can get the new task page" do
-    skip
+    
     
     # Act
     get new_task_path
@@ -61,7 +61,7 @@ end
 
 describe "create" do
   it "can create a new task" do
-    skip
+    
     
     # Arrange
     task_hash = {
@@ -80,7 +80,7 @@ post tasks_path, params: task_hash
 new_task = Task.find_by(name: task_hash[:task][:name])
 expect(new_task.description).must_equal task_hash[:task][:description]
 
-expect(new_task.completed).must_equal task_hash[:task][:completed]
+expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
 
 must_respond_with :redirect
 must_redirect_to task_path(new_task.id)
