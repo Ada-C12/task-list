f4bb0568c160676c8ee2e7e0d1f5a1211b708265
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-02 20:45:16 -0700

    Made changes to tests based on instructions from instructors.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -9,18 +9,12 @@ describe TasksController do
   # Tests for Wave 1
   describe "index" do
     it "can get the index path" do
-      # Act
       get "/tasks"
-      
-      # Assert
       must_respond_with :success
     end
     
     it "can get the root path" do
-      # Act
       get root_path
-      
-      # Assert
       must_respond_with :success
     end
   end
@@ -28,42 +22,25 @@ describe TasksController do
   # Unskip these tests for Wave 2
   describe "show" do
     it "can get a valid task" do
-      # skip
-      # Act
       get task_path(task.id)
-      
-      # Assert
       must_respond_with :success
     end
     
     it "will redirect for an invalid task" do
-      # skip
-      # Act
       get task_path(-1)
-      
-      # Assert
       must_respond_with :redirect
-      expect(flash[:error]).must_equal "Could not find task with id: -1"
     end
   end
   
   describe "new" do
     it "can get the new task page" do
-      # skip
-      
-      # Act
       get new_task_path
-      
-      # Assert
       must_respond_with :success
     end
   end
   
   describe "create" do
     it "can create a new task" do
-      # skip
-      
-      # Arrange
       task_hash = {
         task: {
           name: "new task",
@@ -72,28 +49,22 @@ describe TasksController do
         },
       }
       
-      # Act-Assert
       expect {
         post tasks_path, params: task_hash
       }.must_change "Task.count", 1
       
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
-      # confused about due_date, completed, completed
-      # expect(new_task.due_date.to_time.to_i).must_equal task_hash[:task][:due_date].to_i
-      # expect(new_task.completed).must_equal task_hash[:task][:completed]
+      expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
       
       must_respond_with :redirect
-      #must redirect to tasks_path rather than task_path
       must_redirect_to tasks_path
     end
   end
   
-  # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
       get edit_task_path(task)
-      # Assert
       must_respond_with :success
       
     end
@@ -104,21 +75,18 @@ describe TasksController do
     end
   end
   
-  # Uncomment and complete these tests for Wave 3
+  
   describe "update" do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
     it "can update an existing task" do
-      # Your code here
-      # Arrange
+      
       task_hash = {
         task: {
           name: "updated task",
           description: "update new task description"
         }
       }
-      
-      # Act-Assert
       
       patch update_task_path(task), params: task_hash
       
@@ -146,7 +114,6 @@ describe TasksController do
   # Complete these tests for Wave 4
   describe "destroy" do
     # Your tests go here
-    
   end
   
   # Complete for Wave 4
