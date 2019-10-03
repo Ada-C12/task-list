dfb9059d9e4fa4443704e9af208100c5ec7a5854
dora1405 <li.dora05@gmail.com>
2019-10-02 21:07:22 -0700

    Wave 3 complete. Wrote tests and passed tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 end
