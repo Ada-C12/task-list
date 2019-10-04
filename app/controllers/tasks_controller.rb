7bf05cee131e20d255e9b40e35ffc602ceb0a0f9
Brianna Kemp <kemp.bri@gmail.com>
2019-10-04 11:02:35 -0700

    Index view working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,18 @@
-TASKS = [
-  { title: "Hidden Figures", action: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
-  { title: "Kindred", action: "Octavia E. Butler"}
+# TASKS = [
+#   { title: "Hidden Figures", action: "Margot Lee Shetterly"},
+#   { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
+#   { title: "Kindred", action: "Octavia E. Butler"}
+# ]
+TASKS = [
+  { title: "Hidden Figures", action: "Margot Lee Shetterly"},
+  { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
+  { title: "Kindred", action: "Octavia E. Butler"}
 ]
 
-class TasksController < ApplicationController
-    def index
-        @tasks = TASKS
-    end
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = TASKS
+  end
+  
 end
