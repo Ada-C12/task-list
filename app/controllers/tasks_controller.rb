1ef694bbccb23672c99b5c1f710f6b8bd8a889d9
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-04 16:54:00 -0700

    added task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: nil)
+    @task = Task.new( task_params )
     
     if @task.save
       redirect_to task_path(@task.id)
@@ -48,18 +48,15 @@ class TasksController < ApplicationController
       redirect_to root_path
       return
     end
-    
-    @task.name = params[:task][:name]
-    @task.description = params[:task][:description]
-    @task.completion_date = params[:task][:completion_date]
-    
-    if @task.save
+
+    if @task.update( task_params )
       redirect_to task_path(@task.id)
       return
     else
       render edit_task_path(@task.id)
       return
     end
+    
   end
   
   def destroy
@@ -86,6 +83,12 @@ class TasksController < ApplicationController
       redirect_to tasks_path
       return
     end
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completion_date)
   end
   
 end
