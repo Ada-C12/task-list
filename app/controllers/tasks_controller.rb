371b623a5ea806499aa1584bd3f22d9f0dcc9f3d
Brianna Kemp <kemp.bri@gmail.com>
2019-10-04 15:15:59 -0700

    Added completion date default value

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
   end
@@ -21,7 +21,7 @@ class TasksController < ApplicationController
     @task = Task.new( name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date] )
     
     if @task.save
-      redirect_to book_path(@book.id)
+      redirect_to task_path(@task.id)
     else
       render new_task_path
     end
