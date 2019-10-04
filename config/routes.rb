69cb60c4eeecc01e97075a36945b495989e1f772
Yasmin <yasmin312002@hotmail.com>
2019-10-04 11:59:21 -0700

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Routes that operate on the task collection
+  #set the homepage route
   root 'tasks#index'
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  post '/tasks', to: 'tasks#create'
+  #resources :tasks, only: [:index, :show]
+  #also can use as resources :tasks, except: :index
+  
+  resources :tasks #covered all the lines below:
 
-  # Routes that operate on individual tasks
-     #get '/tasks/:id', to: 'tasks#show', as: 'task'
-     get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-     patch '/tasks/:id', to: 'tasks#update'
-    # delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # post '/tasks', to: 'tasks#create'
+
+  # # Routes that operate on individual tasks
+  #   #get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #   patch '/tasks/:id', to: 'tasks#update'
+  #   delete '/tasks/:id', to: 'tasks#destroy'
   
   
 end
