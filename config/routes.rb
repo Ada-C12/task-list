bd0a161da576756e99b0b5035467e03be2364ec5
brilatimer <brihiramine@gmail.com>
2019-10-04 10:59:34 -0700

    used resources instead of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
   root 'tasks#index'
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
+  resources :tasks
   
-  
-  
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
