efcc3666c7d89b9643685935c48b11fb31c561a1
Brianna Kemp <kemp.bri@gmail.com>
2019-10-04 10:51:33 -0700

    Added view for index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 TASKS = [
-  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  { title: "Kindred", author: "Octavia E. Butler"}
+  { title: "Hidden Figures", action: "Margot Lee Shetterly"},
+  { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
+  { title: "Kindred", action: "Octavia E. Butler"}
 ]
 
 class TasksController < ApplicationController
