9108ce474d17a60b507fc4b3907dee30fe087ae5
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-04 10:54:11 -0700

    updated routes.rb to use resources method

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,18 @@ Rails.application.routes.draw do
   
   root "tasks#index"
   
+  resources :tasks, except: [:update]
+  
   # Routes that operate on the task collection
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
   
   # Routes that operate on individual tasks
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id', to: 'tasks#update'
   patch '/tasks/:id/complete', to: 'tasks#mark_complete', as: 'mark_complete'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # delete '/tasks/:id', to: 'tasks#destroy'
   
 end
