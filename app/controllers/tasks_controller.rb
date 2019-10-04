b0e5316d24353bfecc2c6883af434ceb67bf5640
Rinostar <codingrinostar@gmail.com>
2019-10-04 15:32:10 -0700

    wave_2 & 3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,13 +18,57 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], due: params[:task][:due])
+    @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to root_path
       return
     else
       render :new
       return
     end
   end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
+    if@book.nil?
+      head :not_found
+      return
+    end
+  end 
+
+  def update
+    @task = Taks.find_by(id: params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+
+    if task.nil?
+      head :not_found
+      return
+    end
+
+    @task.destroy
+
+    redirect_to root_path
+    return
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(
+      :name, 
+      :description, 
+      :due)
+  end
+
 end
