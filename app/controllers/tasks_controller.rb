60ad6183022a4da4bbf5e75f4f8f9698a5eb401d
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-04 15:38:43 -0700

    Path for toggle_complete is correct

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   
   def show
     task_id = params[:id].to_i
-    @task = Task.find_by(id:task_id)
+    @task = Task.find_by(id: task_id)
     if @task.nil?
       flash[:error] = "Could not find task with id: #{task_id}"
       redirect_to tasks_path
@@ -64,27 +64,37 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
   
-  def toggle_complete 
-    # task_id = params[:id]
-    # @task = Task.find_by(id: task_id)
-  end
-  
-  def mark_complete
-    # task_id = params[:id]
-    # @task = Task.find_by(id: task_id)
-    # @task.complete = Time.now
-    # @task.save
+  def toggle_complete
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    @task[:completed] = Time.now
+    
+    redirect_to root_path
     
   end
   
-  def un_check
+  def toggle_incomplete
+    # ask_id = params[:id]
+    # @task = Task.find_by(:id task_id)
+    
+    # if @task.nil?
+    #   flash[:error] = "Error, not a task."
+    #   redirect_to root_path
+    #   return
+    # end
+    # @task[:completed] = nil
+    # redirect_to root_path
   end
+  #   toggle complete is 
+  # it is two patch requests 
+  # if `complete` is set to nil, then patch request complete to "time-now"
+  # Time: right now 
+  # or time nil
   
   
   private
   def task_params
     return params.require(:task).permit(:name, :description, :id)
-    
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  root 'tasks#index'
   resources :tasks, except: [:update]
   patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
   
-  root 'tasks#index'
+  
+  patch '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete', as: 'toggle_complete'
+  patch '/tasks/:id/toggle_incomplete', to: 'tasks#toggle_incomplete', as: 'toggle_incomplete'
+  
+  
+  
+  
 end
