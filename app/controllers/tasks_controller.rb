a70b802658304643822107b6fd35116c6611c700
Brianna Kemp <kemp.bri@gmail.com>
2019-10-04 11:26:16 -0700

    Created database and added two records

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,7 @@
-# TASKS = [
-#   { title: "Hidden Figures", action: "Margot Lee Shetterly"},
-#   { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
-#   { title: "Kindred", action: "Octavia E. Butler"}
-# ]
-TASKS = [
-  { title: "Hidden Figures", action: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", action: "Sandi Metz"},
-  { title: "Kindred", action: "Octavia E. Butler"}
-]
-
 class TasksController < ApplicationController
   
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
   
 end
