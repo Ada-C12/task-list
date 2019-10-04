bae3a39ac35011f3a1b6b758f0e77ae5031803bd
Brianna Kemp <kemp.bri@gmail.com>
2019-10-04 10:47:55 -0700

    Added controller index action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
-class TasksController < ApplicationController
+TASKS = [
+  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+  { title: "Kindred", author: "Octavia E. Butler"}
+]
+
+class TasksController < ApplicationController
+    def index
+        @tasks = TASKS
+    end
 end
