5667d42bcccc1f6370811f9297c0e5808d3d8f1e
Janice Huang <janicehuang@gmail.com>
2019-10-04 12:31:44 -0700

    migration to remove assignment_date column from model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_222713) do
+ActiveRecord::Schema.define(version: 2019_10_04_192713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_222713) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
-    t.date "assignment_date"
     t.date "completion_date"
   end
 
