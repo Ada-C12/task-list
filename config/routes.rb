7349d53159a657df824a25d0f4418f5c5945dd5a
m.dove@live.com <m.dove@live.com>
2019-10-04 11:17:46 -0700

    Deleted and re-added the completed column in order to change the datatype to Datetime.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root :to => "tasks#index"
+  # resources :tasks, except: [:edit]
   get "/tasks", to: "tasks#index", as: "tasks"
   get "/tasks/new", to: "tasks#new", as: "new_task"
   post "/tasks", to: "tasks#create"
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   get "tasks/:id", to: "tasks#show", as: "task"
   # get "/:name", to: "tasks#show"
   patch "/tasks/:id", to: "tasks#update", as: "update_task"
-  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+
   delete "/tasks/:id", to: "tasks#destroy"
 end
---
+++ db/migrate/20191004175223_delete_completed.rb
@@ -0,0 +1,5 @@
+class DeleteCompleted < ActiveRecord::Migration[5.2]
+  def change
+    remove_column(:tasks, :completed)
+  end
+end
