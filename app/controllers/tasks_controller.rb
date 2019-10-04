9411ef74a53669271159a05318273f148ab7b0b3
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-04 10:31:11 -0700

    Added 'complete' boolean to schema

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: nil)
     if @task.save
       redirect_to task_path(@task)
       return
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_232235) do
+ActiveRecord::Schema.define(version: 2019_10_04_172853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_232235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "completion_date"
+    t.boolean "complete"
   end
 
 end
