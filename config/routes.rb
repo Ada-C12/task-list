8ebd21d3797c39f1c66962bbb96e28de6411420f
emaust <erikamaust@gmail.com>
2019-10-05 16:21:06 -0700

    Updated task_controller and routes to include show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root :to => "tasks#index"
-  get '/tasks', to: 'tasks#index'
+  
+  get '/tasks', to: 'tasks#index', as: 'index'
+  
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  
+  # get '/tasks/new, to: tasks#new'
+  
+  
+  
 end
