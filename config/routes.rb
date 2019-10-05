44ae18e31cda362b15a5893ef95010175c5caa12
Dianna Tingg <dtingg@gmail.com>
2019-10-05 10:58:24 -0700

    added strikethrough for completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
-  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
-  patch '/tasks/:id/not_complete', to: 'tasks#not_complete', as: 'not_complete_task'
+  root to: "tasks#index"
+  patch "/tasks/:id/complete", to: "tasks#complete", as: "complete_task"
+  patch "/tasks/:id/not_complete", to: "tasks#not_complete", as: "not_complete_task"
   resources :tasks, except: [:update]
-  patch '/tasks/:id', to: 'tasks#update' 
+  patch "/tasks/:id", to: "tasks#update"
 end
