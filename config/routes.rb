22997c13d45611e54fd8f3638ed562198a456196
m.dove@live.com <m.dove@live.com>
2019-10-05 22:38:52 -0700

    Added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  root :to => "tasks#index"
+  root to: "tasks#index"
   # resources :tasks, except: [:edit]
-  get "/tasks", to: "tasks#index", as: "tasks"
-  get "/tasks/new", to: "tasks#new", as: "new_task"
-  post "/tasks", to: "tasks#create", as: "create_task"
+  # get "/tasks", to: "tasks#index", as: "tasks"
+  # get "/tasks/new", to: "tasks#new", as: "new_task"
+  # post "/tasks", to: "tasks#create", as: "create_task"
   patch "/tasks/:id/toggle_completed", to: "tasks#toggle_completed", as: "toggle_completed_task"
-  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
-  get "/tasks/:id", to: "tasks#show", as: "task"
-  # patch "tasks/:id", to "tasks#index", as: "toggle_complete_task"
-  # get "/:name", to: "tasks#show"
-  #I need two routes for my toggle thingy. One for toggle on and one for toggle off
-  patch "/tasks/:id", to: "tasks#update", as: "update_task"
+  # get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # get "/tasks/:id", to: "tasks#show", as: "task"
 
-  delete "/tasks/:id", to: "tasks#destroy"
+  #I need two routes for my toggle thingy. One for toggle on and one for toggle off
+  # patch "/tasks/:id", to: "tasks#update", as: "update_task"
+
+  # delete "/tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
