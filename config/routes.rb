25842785075bbc039daec91882cf47ebe8760d25
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-06 14:29:07 -0700

    deleted extra commented-out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #verb 'path', to: 'controller#action'
-  #routes that operate on the tasks collection
-
   root 'tasks#index'
   patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
   resources :tasks
-#   root 'tasks#index'
-#   get '/tasks', to: 'tasks#index', as: 'tasks'
-#   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-#   post '/tasks', to: 'tasks#create'
-
-#   #routes that operate on individual tasks
-#   get '/tasks/:id', to: 'tasks#show', as: 'task'
-#   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-#   patch '/tasks/:id', to: 'tasks#update'
-#   delete '/tasks/:id', to: 'tasks#destroy'
 end
