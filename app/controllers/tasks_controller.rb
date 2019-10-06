af203f82d1aaeb8fe08a3fe2ecfe5e420b82ea0c
emaust <erikamaust@gmail.com>
2019-10-05 19:36:17 -0700

    Create new task working, tests do not pass - need to change completion_date to completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,35 @@ class TasksController < ApplicationController
         task_id = params[:id].to_i
         @task = Task.find_by(id: task_id)
         if @task.nil?
-            head :not_found
+            head :redirect
             return
         end
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new( task_params )
+        
+        if @task.save
+            redirect_to task_path(@task.id)
+        else
+            render new_book_path
+        end
+    end
+    
+    
+    
+    private 
+    def task_params
+        # The responsibility of this method is to return "strong params"
+        # .require is used when we use form_with and a model, and therefore our expected form data has the "book" hash inside of it
+        # .permit takes in a list of names of attributes to allow... (aka the new Book form has title, author, description)
+        return params.require(:task).permit(:name, :description, :completion_date)
+        
+        # Remember: If you ever update the database, model, and form, this will also need to be updated
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  root :to => "tasks#index"
+  root to: "tasks#index"
   
-  get '/tasks', to: 'tasks#index', as: 'index'
+  resources :tasks
+  # get '/tasks', to: 'tasks#index', as: 'index'
   
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  
+  # post 'tasks/', to: 'tasks#create', as: 'tasks'
+  
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
   
   # get '/tasks/new, to: tasks#new'
   
