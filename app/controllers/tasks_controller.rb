8422e7d8fbdad8f1e62c64e1ee6db43e6b58360d
KKennedyCodes <holmstedtk@gmail.com>
2019-10-06 04:53:56 -0700

    made changes to form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,57 @@
-TASKS = [
-  { name: "Hidden Figures", description: "Margot Lee Shetterly"},
-  { name: "Practical Object-Oriented Design in Ruby", description: "Sandi Metz"},
-  { name: "Kindred", description: "Octavia E. Butler"}
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @task = Task.all
+  end
+  
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+      return
+    else
+      render :new
+      return
+    end  
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.update(task_params)
+    # if @task.update
+    redirect_to root_path
+    #   return
+    # else
+    #   render :new
+    #   return
+    # end
+  end
+  def destroy
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    @task.delete
+    redirect_to root_path
   end
 end
 
+private
+
+def task_params
+  return params.require(:task).permit(:name, :title, :description, :completed)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
+  root 'tasks#index' #redirects home to index
+  
+  # Task Collection
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  
+  # Individual Tasks
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
