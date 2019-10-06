521c2724e105c79f68bdcafa8bc933bbbb3d76f3
emaust <erikamaust@gmail.com>
2019-10-05 19:42:12 -0700

    Change of heart, tests changed from completed to completion_date

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe TasksController do
   let (:task) {
   Task.create name: "sample task", description: "this is an example for a test",
-  completed: Time.now + 5.days
+  completion_date: Time.now + 5.days
 }
 
 # Tests for Wave 1
@@ -66,7 +66,7 @@ describe "create" do
     task: {
     name: "new task",
     description: "new task description",
-    completed: nil,
+    completion_date: nil,
   },
 }
 
@@ -77,7 +77,7 @@ post tasks_path, params: task_hash
 
 new_task = Task.find_by(name: task_hash[:task][:name])
 expect(new_task.description).must_equal task_hash[:task][:description]
-expect(new_task.completed).must_equal task_hash[:task][:completed]
+expect(new_task.completion_date).must_equal task_hash[:task][:completion_date]
 
 must_respond_with :redirect
 must_redirect_to task_path(new_task.id)
