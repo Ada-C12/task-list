b0d1dc74ba1e57e3af001f43495c01044a5e1d25
brilatimer <brihiramine@gmail.com>
2019-10-06 20:28:19 -0700

    made toggle complete option with date on main page and each task, added name on individual task pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
   root 'tasks#index'
   resources :tasks
-  
+  get '/tasks/status/:id', to: 'tasks#toggle_complete', as: 'task_status'
+
   # get '/tasks', to: 'tasks#index'
   # get '/tasks/new', to: 'tasks#new', as: 'new_task'
   # post '/tasks', to: 'tasks#create'
