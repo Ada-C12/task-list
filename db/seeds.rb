bd459872ec7ac5e46dccfa7379aa41e93f9c9c79
Eve Le <nguyenthao.lee@gmail.com>
2019-10-10 09:57:07 -0700

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tasks = [
+    {
+        name: "Task1",
+        description: "Task 1 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task2",
+        description: "Task 2 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task3",
+        description: "Task 3 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task4",
+        description: "Task 4 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task5",
+        description: "Task 5 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task6",
+        description: "Task 6 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task7",
+        description: "Task 7 description",
+        completion_date: Date.current
+    },
+    {
+        name: "Task8",
+        description: "Task 8 description",
+        completion_date: Date.current
+    }
+]
+
+task_failures = []
+tasks.each do |input_task|
+  task = Task.new(input_task)
+  successful = task.save
+  if successful
+    puts "Created task: #{task.inspect}"
+  else
+    task_failures << task
+    puts "Failed to save task: #{task.inspect}"
+  end
+end
