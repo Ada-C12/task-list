12d2a9c275749d4d543e63ad015f224d4acbaf72
Michaela Morrow <morrow.michaela@gmail.com>
2019-10-10 09:53:50 -0700

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def random_time
+  Time.at(rand * Time.now.to_i)
+end
+
+tasks = [
+  {name: "The First Task", description: "", completion_date: random_time},
+  {name: "Go to Brunch", description: ""},
+  {name: "Go to Lunch", description: "", completion_date: random_time},
+  {name: "Go to Second Lunch", description: ""},
+  {name: "Play Video Games", description: "", completion_date: random_time},
+  {name: "High Five Somebody You Don't Know", description: "", completion_date: random_time},
+  {name: "Plant Flowers", description: "", completion_date: random_time},
+  {name: "Call Mom", description: ""},
+  {name: "She worries, you know.", description: ""},
+  {name: "Nap.", description: "", completion_date: random_time},
+]
+
+tasks.each do |task|
+  Task.create task
+end
