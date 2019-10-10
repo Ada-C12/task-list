d5a3a0ce4c7c5ea1923af738c3f2df71950dfbc1
dora1405 <li.dora05@gmail.com>
2019-10-10 09:53:43 -0700

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "Laundry", description: "drop off at John Doe LLC", completed: nil)
+Task.create(name: "Dry cleaning", description: "drop off at Bob Doe LLC", completed: nil)
